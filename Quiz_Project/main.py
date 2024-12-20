eeda0278ca74f134472a8795832b712a4960afca
Sangit <sangeet.himire@gmail.com>
2024-12-20 15:45:46 +0545

    quiz_project
    a stupid quiz program created using classes and objects
    
     ██████╗ ██╗   ██╗██╗███████╗███████╗██╗ ██████╗ █████╗ ██╗     
    ██╔═══██╗██║   ██║██║╚══███╔╝╚══███╔╝██║██╔════╝██╔══██╗██║     
    ██║   ██║██║   ██║██║  ███╔╝   ███╔╝ ██║██║     ███████║██║     
    ██║▄▄ ██║██║   ██║██║ ███╔╝   ███╔╝  ██║██║     ██╔══██║██║     
    ╚██████╔╝╚██████╔╝██║███████╗███████╗██║╚██████╗██║  ██║███████╗
     ╚══▀▀═╝  ╚═════╝ ╚═╝╚══════╝╚══════╝╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝

---
+++ Quiz_Project/main.py
@@ -0,0 +1,32 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text,question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+logo = """
+
+ ██████╗ ██╗   ██╗██╗███████╗███████╗██╗ ██████╗ █████╗ ██╗     
+██╔═══██╗██║   ██║██║╚══███╔╝╚══███╔╝██║██╔════╝██╔══██╗██║     
+██║   ██║██║   ██║██║  ███╔╝   ███╔╝ ██║██║     ███████║██║     
+██║▄▄ ██║██║   ██║██║ ███╔╝   ███╔╝  ██║██║     ██╔══██║██║     
+╚██████╔╝╚██████╔╝██║███████╗███████╗██║╚██████╗██║  ██║███████╗
+ ╚══▀▀═╝  ╚═════╝ ╚═╝╚══════╝╚══════╝╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝
+                                                                
+
+"""
+
+
+print(logo)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("Congratulations on Completion of this Quiz")
+print(f"Your Final Score Was: {quiz.score}/{quiz.question_number}")
