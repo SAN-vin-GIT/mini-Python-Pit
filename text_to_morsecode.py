295a045ab2e0dc801c508aec1217938910c59c04
Sangit <sangeet.himire@gmail.com>
2025-02-08 12:55:54 +0545

    text_to_morsecode.py

---
+++ text_to_morsecode.py
@@ -0,0 +1,19 @@
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.', ' ': ' '
+}
+
+def text_to_morse(text):
+    text = text.upper()
+    morse_code = ' '.join(MORSE_CODE_DICT[char] for char in text if char in MORSE_CODE_DICT)
+    return morse_code
+
+user_text = input("Enter text: ")
+morse_output = text_to_morse(user_text)
+print("Morse Code:", morse_output)
